2be3d1cb2f40a9c0878e7bd0e37f4b3bb76c5843
jhonrymat <jhonrymat@gmail.com>
2025-12-09 09:54:00 -0500

    fix: Correct URL format in AJAX request for envio status update

--- resources/views/envios/monitor.blade.php
+++ resources/views/envios/monitor.blade.php
@@ -74,7 +74,7 @@
 
         function actualizarEstado() {
             $.ajax({
-                url: `envios/${envioId}/status`,
+                url: `/envios/${envioId}/status`,
                 method: 'GET',
                 success: function(response) {
                     if (response.batch) {
