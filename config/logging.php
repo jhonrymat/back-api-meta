1c1017999e3d15db5761fa08bbd608fe8a7fcc3c
jhonrymat <jhonrymat@gmail.com>
2025-12-09 15:26:49 -0500

    fix: Add permission setting for file and logging cache stores

--- config/logging.php
+++ config/logging.php
@@ -65,6 +65,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 7,
+            'permission' => 0764,
         ],
 
         'slack' => [
